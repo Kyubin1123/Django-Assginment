4dad93a664f3e417c6cade75cf1781f45f2ebb34
Kyubin1123 <michelle010929@gmail.com>
2024-11-27 14:38:35 +0900

    chapter_02 과제 업로드

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.http import Http404
 from django.shortcuts import render
 from django.urls import path
 from fake_db import user_db
+from todo.views import todo_list, todo_info
 
 _db = user_db
 
@@ -32,10 +33,13 @@ def user_info(request, user_id):
         raise Http404('User not found')
     info = _db[user_id]
     return render(request, template_name = 'user_info.html', context = {'data':info})
-
+#
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", user_list, name='user_list'),
     path("users/<int:user_id>/", user_info, name='user_info'),
+
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
 ]
