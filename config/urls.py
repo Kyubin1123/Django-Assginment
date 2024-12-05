94e4519c759545fa96245939117e799764021043
Kyubin1123 <michelle010929@gmail.com>
2024-12-05 18:47:55 +0900

    마무리작업

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.http import Http404
 from django.shortcuts import render
-from django.urls import path
+from django.urls import path, include
 from fake_db import user_db
-from todo.views import todo_list, todo_info
+from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete
+from users import views as user_views
 
 _db = user_db
 
@@ -39,7 +40,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", user_list, name='user_list'),
     path("users/<int:user_id>/", user_info, name='user_info'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', user_views.login, name='login'),
+    path('accounts/signup/', user_views.sign_up, name='sign_up'),
 
     path('todo/', todo_list, name='todo_list'),
+    path('todo/create/', todo_create, name='todo_create'),
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path('todo/<int:todo_id>/update/', todo_update, name='todo_update'),
+    path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
 ]
