491c9a5e3e81a5d150a5925c3990559bd85ab53f
Kyubin1123 <michelle010929@gmail.com>
2024-12-09 17:13:46 +0900

    feat:chapter_05 과제 업로드

--- config/urls.py
+++ config/urls.py
@@ -45,9 +45,5 @@ urlpatterns = [
     path('accounts/login/', user_views.login, name='login'),
     path('accounts/signup/', user_views.sign_up, name='signup'),
 
-    path('todo/', todo_list, name='todo_list'),
-    path('todo/create/', todo_create, name='todo_create'),
-    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
-    path('todo/<int:todo_id>/update/', todo_update, name='todo_update'),
-    path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
+    path('cbv/', include('todo.urls')),
 ]
